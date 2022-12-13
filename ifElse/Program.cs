dcb15ec74c4f4d6369ff65ae5f9b751217b77bfc
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-14 01:33:22 +0530

    ifElse

---
+++ ifElse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ifElse
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x1 = 50;
+            int x2 = 100;
+            if (x1 > x2)
+            {
+                Console.WriteLine("X1 is greater");
+            }
+            else
+            {
+                Console.WriteLine("X2 is greater");
+            }
+
+        }
+    }
+    
+}
+
+        
