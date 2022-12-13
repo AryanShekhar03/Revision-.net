42113865ea1f00e845b1940e9ebd69bf11300e8e
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-13 19:41:04 +0530

    Polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Polymorphism
+{
+
+    public class Human
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Human eating");
+        }
+    }
+    public class Dog : Human
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Dog eating");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Human h = new Dog();
+            h.eat();
+        }
+    }
+}
