911d3a3853583ac4d728191c80be728573f4a53b
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-13 19:14:51 +0530

    Inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Inheritance
+{
+
+    public class Employee
+    {
+        public double salary = 50000;
+    }
+    public class Programmmer : Employee
+    {
+        public double bonus = 1000;
+    }
+     public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Programmmer programmmer = new Programmmer();
+            Console.WriteLine(programmmer.salary + " " + programmmer.bonus);
+        }
+    }
+}
