44e0dd4eb13d202c01b60a2add4ebb8e705837e2
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-13 23:53:20 +0530

    loops iteration

--- loops iteration/Program.cs
+++ loops iteration/Program.cs
@@ -2,11 +2,56 @@
 
 namespace loops_iteration
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        public static void Main(string[] args)
+        {   //while loop
+            //int num = 1;
+            //while (num <= 10)
+            //{
+            //    Console.WriteLine("The number is {0}", num);
+            //    num++;
+            //}
+            //Console.ReadLine();
+
+            //DO WHILE LOOP
+            //int num = 1;
+            //do
+            //{
+            //    Console.WriteLine("The number is {0}", num);
+            //    num++;
+            //} while (num <= 0);
+
+            //Console.ReadLine();
+
+
+            //fOR LOOP
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("The iteration number is " + i);
+            //}
+
+            //Console.ReadLine();
+
+
+            //For Each loop
+            //    string[] daysOfWeek = new string[] {
+            //    "Monday",
+            //        "Tuesday",
+            //        "Wednesday",
+            //        "Thursday",
+            //        "Friday"
+            //};
+            //    foreach (string Day in daysOfWeek)
+            //    {
+            //        Console.WriteLine("The Day is : {0}", Day);
+            //    }
+
+
+            //    Console.ReadLine();
+            //}
         }
     }
 }
+ 
+
