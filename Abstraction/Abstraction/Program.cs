fa712ae90427d27376b688a381faaa5bbd472dc9
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-13 17:50:07 +0530

    Abstraction

---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Abstraction
+{ 
+     public abstract class Shape
+{
+    public abstract void draw();
+}
+
+public class Square : Shape
+{
+    public override void draw()
+    {
+        Console.WriteLine("Square");
+    }
+}
+public class Triangle : Shape
+{
+    public override void draw()
+    {
+        Console.WriteLine("Triangle");
+    }
+}
+
+   public class program
+    {
+        public static void Main(string[] args)
+        {
+            Shape shape = new Square();
+            shape.draw();
+            Shape shap = new Triangle();
+            shap.draw();
+        }
+    }
+}
+
