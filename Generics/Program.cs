1d650bca0b13ac7bacbaf7164c482a7b918312f6
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-15 02:24:27 +0530

    GENERICS

---
+++ Generics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Generics
+{
+    public class Program<T>
+    {
+        private T data;
+
+        // using properties
+        public T value
+        {
+
+            // using accessors
+            get
+            {
+                return this.data;
+            }
+            set
+            {
+                this.data = value;
+            }
+        }
+        class Test
+        {
+             static void Main(string[] args)
+            {
+                // instance of string type
+                Program<string> name = new Program<string>();
+                name.value = "GeeksforGeeks";
+
+                // instance of float type
+                Program<float> version = new Program<float>();
+                version.value = 5.0F;
+
+                // display GeeksforGeeks
+                Console.WriteLine(name.value);
+
+                // display 5
+                Console.WriteLine(version.value);
+            }
+        }
+    }
+}
