322b8525a19357830c530d0fafabe136bd31ea60
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-15 00:25:07 +0530

    hashtable

---
+++ Hashtable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTable
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("1", "Aaryan");
+            ht.Add("2", "shekhar");
+            foreach (string key in ht.Keys)
+            {
+                Console.WriteLine(key + " " + ht[key]);
+            }
+        }
+    }
+}
