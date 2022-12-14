c427101f896282c5eeb51da31d8fb4058d8b01de
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-14 17:04:50 +0530

    stack

---
+++ stack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace stack
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack list = new Stack();
+            list.Push(1);
+            list.Push(2);
+            list.Push("abc");
+            list.Pop();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
