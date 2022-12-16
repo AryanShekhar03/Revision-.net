98e87501c781cc049bc3336ff38ad8c9cc476c2e
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-16 11:53:37 +0530

    regex

---
+++ Regex/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+using System;
+
+
+namespace Rege
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Name: ");
+                string name = Console.ReadLine();
+                string pattrn = "^[A-Z]{1,}[A-Za-z]{2,}";
+                if (Regex.IsMatch(name, pattrn))
+                {
+                    Console.WriteLine("valid name " + name);
+                }
+                else
+                {
+                    Console.WriteLine("not valid");
+                };
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
