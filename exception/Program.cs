dadabf601a420ad6c4f34f001412ed5bfc870469
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-16 18:30:39 +0530

    exception

---
+++ exception/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exception
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+        }
+    }
+}
