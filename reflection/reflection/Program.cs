cf549ff128f0c294bb07dba16d045495b0b675ca
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-18 13:10:30 +0530

    reflection

---
+++ reflection/reflection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(string);
+
+            // Use Reflection to find about
+            // any sort of data related to t
+            Console.WriteLine("Name : {0}", t.Name);
+            Console.WriteLine("Full Name : {0}", t.FullName);
+            Console.WriteLine("Namespace : {0}", t.Namespace);
+            Console.WriteLine("Base Type : {0}", t.BaseType);
+        }
+    }
+}
