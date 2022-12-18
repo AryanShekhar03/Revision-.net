cf58f79ba3b86b083e25844df2a0fb36a8c95ecc
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-18 12:51:33 +0530

    lambda

---
+++ lambda/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace lambd
+{
+    class Program
+    {
+        string Pattern = "^[A-Z]{1,}[a-z]{3,}$";
+        public string CheckName(string name) => Regex.IsMatch(name, Pattern) ? "Name is Valid" : "Name is not Valid";
+    }
+
+
+
+    class lamda { 
+    public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            Program valid = new Program();
+            string user = valid.CheckName(name);
+            if (user == "Name is Valid")
+            {
+                Console.WriteLine(user);
+            }
+            else
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
