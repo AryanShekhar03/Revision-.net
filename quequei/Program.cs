c3d6facdb97f93cba428345e50680a23b208cf75
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-21 01:30:18 +0530

    quequei

---
+++ quequei/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace quequei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListQueueCaller();
+
+            Console.ReadLine();
+        }
+
+        public static void LinkedListQueueCaller()
+        {
+            LinkListQueue myQueue = new LinkListQueue();
+
+            myQueue.Enqueue(10);
+            myQueue.Enqueue(20);
+            myQueue.Enqueue(30);
+            myQueue.Enqueue(40);
+
+            myQueue.PrintQueue();
+
+            myQueue.Dequeue();
+            myQueue.Enqueue(50);
+
+            myQueue.PrintQueue();
+            myQueue.Dequeue();
+            myQueue.PrintQueue();
+        }
+    }
+}
