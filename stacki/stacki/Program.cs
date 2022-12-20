5e205686991155ea484409e143df244821ed3990
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-21 01:21:58 +0530

    stack

---
+++ stacki/stacki/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace stacki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStackCaller();
+            Console.ReadLine();
+
+            public static void LinkedListStackCaller()
+            {
+                LinkListStack myStack = new LinkListStack();
+
+                myStack.Push(10);
+                myStack.Push(20);
+                myStack.Push(30);
+                myStack.Push(40);
+                myStack.PrintStack();
+                myStack.Peek();
+                myStack.Pop();
+                myStack.Peek();
+                myStack.Pop();
+                myStack.PrintStack();
+            }
+        }
+    }
+}
