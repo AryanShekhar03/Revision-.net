4ac33fe130e665d3985af3e42f94e09d5ec006f5
AaryanShekhar <aaryan.shekhar@doubtnut.com>
2022-12-22 09:33:31 +0530

    Ado.net

---
+++ ADo.net/ADo.net/Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADo.net
+{
+   public  class Model
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string City { get; set; }
+    }
+}
